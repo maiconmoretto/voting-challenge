4dea3cd252b50f83753752966e01a2a8ca1cbcfa
maiconmoretto <maiconmorettos@gmail.com>
2021-03-18 21:32:54 -0300

    Created micro service for rabbitMQ

---
+++ rabbit-mq/src/main/java/com/br/service/Sender.java
@@ -0,0 +1,34 @@
+package com.br.service;
+
+import java.io.IOException;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.TimeoutException;
+/*
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+
+import com.br.model.Agenda;
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;*/
+
+@Service
+public class Sender {
+/*
+	public void send(Agenda agenda) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		try (Connection connection = factory.newConnection()) {
+			Gson gson = new Gson();
+			String json = gson.toJson(agenda);
+			Channel channel = connection.createChannel();
+			channel.queueDeclare("assemblies", false, false, false, null);
+			channel.basicPublish("", "assemblies", false, null, json.getBytes());
+			System.out.println("Message sent to the RabbitMQ Successfully");
+		} catch (IOException e) {
+			throw new IOException(e);
+		}
+	}*/
+}
