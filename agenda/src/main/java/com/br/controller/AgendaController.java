b1684a3627149d54068e02c9b051aee9cd4f6a32
maiconmoretto <maiconmorettos@gmail.com>
2021-04-01 17:11:33 -0300

    Added Swagger into Agenda service.

--- agenda/src/main/java/com/br/controller/AgendaController.java
+++ agenda/src/main/java/com/br/controller/AgendaController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.br.model.Agenda;
 import com.br.service.AgendaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class AgendaController {
@@ -25,36 +27,43 @@ public class AgendaController {
 	@Autowired
 	private AgendaService service;
 
+	@ApiOperation(value = "It will return list of Agenda")
 	@GetMapping("/api/v1/agenda")
 	public List<Agenda> findAll() {
 		return service.findAll();
 	}
 
+	@ApiOperation(value = "It will return a Agenda by Id")
 	@GetMapping("/api/v1/agenda/{id}")
 	public Agenda findById(@PathVariable int id) {
 		return service.findById(id);
 	}
 
+	@ApiOperation(value = "It will save a Agenda")
 	@PostMapping(path = "/api/v1/agenda/")
 	public Agenda saveAgenda(@RequestBody Agenda agenda) {
 		return service.save(agenda);
 	}
 
+	@ApiOperation(value = "It will update a Agenda")
 	@PutMapping(value = "/api/v1/agenda/")
 	public Agenda update(@RequestBody Agenda agenda) throws  Exception {
 		return service.update(agenda);
 	}
 
+	@ApiOperation(value = "It will delete a Agenda by Id")
 	@DeleteMapping(path = "/api/v1/agenda/{id}")
 	public ResponseEntity<String> deleteById(@PathVariable int id) {
 		return service.deleteById(id);
 	}
-	
+
+	@ApiOperation(value = "It will return list of Agenda closed")
 	@GetMapping("/api/v1/agendas-closed")
 	public List<Agenda> agendasClosed() {
 		return service.agendasClosed();
 	}
 
+	@ApiOperation(value = "It will return a Agenda open")
 	@GetMapping("/api/v1/agenda-open/{idAgenda}")
 	public Agenda agendaOpen(@PathVariable int idAgenda) {
 		return service.agendaOpen(idAgenda);
