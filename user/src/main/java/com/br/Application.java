fce203e5e8e2e5c497be9d5812fe71e67ba2d1d6
maiconmoretto <maiconmorettos@gmail.com>
2021-04-01 18:54:38 -0300

    Added Swagger to User service.

--- user/src/main/java/com/br/Application.java
+++ user/src/main/java/com/br/Application.java
@@ -3,12 +3,25 @@ package com.br;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
  
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.br.controller")).paths(PathSelectors.any()).build();
     }
 }
--- user/src/main/java/com/br/controller/UserController.java
+++ user/src/main/java/com/br/controller/UserController.java
@@ -15,9 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.br.model.User;
 import com.br.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class UserController {
@@ -25,26 +26,31 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
+	@ApiOperation(value = "It will return list of User")
 	@GetMapping("/api/v1/user")
 	public List<User> findAll() {
 		return service.findAll();
 	}
 
+	@ApiOperation(value = "It will return a User by Id")
 	@GetMapping("/api/v1/user/{id}")
 	public User findById(@PathVariable int id) {
 		return service.findById(id);
 	}
 
+	@ApiOperation(value = "It will save a User")
 	@PostMapping(path = "/api/v1/user/")
 	public @ResponseBody ResponseEntity save(@RequestBody User user) {
 		return service.save(user);
 	}
 
+	@ApiOperation(value = "It will update a User")
 	@PutMapping(value = "/api/v1/user/{id}")
 	public ResponseEntity<String> update(@RequestBody User user) {
 		return service.update(user);
 	}
 
+	@ApiOperation(value = "It will delete a User by Id")
 	@DeleteMapping(path = "/api/v1/user/{id}")
 	public ResponseEntity<String> deleteById(@PathVariable int id) {
 		return 	service.deleteById(id);		
