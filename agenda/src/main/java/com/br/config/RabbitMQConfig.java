1397d5d10e3a341c2ab5795e84add99dce21a319
maiconmoretto <maiconmorettos@gmail.com>
2021-04-03 16:22:40 -0300

    Added RabbitMq Listner to Agenda Service.

---
+++ agenda/src/main/java/com/br/config/RabbitMQConfig.java
@@ -0,0 +1,42 @@
+package com.br.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.br.service.RabbitMQListner;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue}")
+    String queueName;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    //create MessageListenerContainer using default connection factory
+    @Bean
+    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory ) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQListner());
+        return simpleMessageListenerContainer;
+
+    }
+
+}
